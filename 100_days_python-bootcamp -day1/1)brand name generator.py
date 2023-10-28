b8ff0e4b1f8d51649ee68399b248a80f5856ba9a
ARAVIND S <aravindofficial2023@gmail.com>
2023-10-28 22:31:39 +0530

    Add files via upload

---
+++ 100_days_python-bootcamp -day1/1)brand name generator.py
@@ -0,0 +1,4 @@
+your_name=input("enter your name :")#prompts the user to input a string(name of a human)
+pet_name=input("enter your pet name :")#prompts the user to input a string(name of pet)
+brand_name=your_name+pet_name
+print(f"!Hola...your brand name is  THE {brand_name} which is looking perfect!!!!")
